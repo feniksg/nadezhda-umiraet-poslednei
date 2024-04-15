a59f15f24abdd616861f0dff62334e9bf0c8cc68
feniksg <33801134+feniksg@users.noreply.github.com>
2024-04-16 02:16:58 +0300

    Add test controller

---
+++ backend/SiteAPI/SiteAPI/Controllers/CustomUserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiteAPI.Data;
+using SiteAPI.Models;
+
+namespace SiteAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomUserController : ControllerBase
+    {
+        private readonly CustomUserDbContext _context; 
+        public CustomUserController(CustomUserDbContext customUserDbContext)
+        {
+            _context = customUserDbContext;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _context.CustomUsers.ToListAsync();
+            
+            if (users == null) {  return NotFound(); }
+            
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CustomUser user)
+        {
+            await _context.CustomUsers.AddAsync(user);
+            await _context.SaveChangesAsync();
+            
+            return Ok(user);
+        }
+    }
+}
