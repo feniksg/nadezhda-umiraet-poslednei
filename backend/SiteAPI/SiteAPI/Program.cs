c7575867e3c9f1fa1875ec1d1a8410f1c22d9e3b
feniksg <n.malysheva.study@gmail.com>
2024-04-16 13:32:06 +0300

    Замена БД на SQLite3

--- backend/SiteAPI/SiteAPI/Program.cs
+++ backend/SiteAPI/SiteAPI/Program.cs
@@ -2,37 +2,41 @@ using Microsoft.EntityFrameworkCore;
 using SiteAPI.Data;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<CustomUserDbContext>(option => option.UseInMemoryDatabase("SiteDB"));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+    private static void Main(string[] args)
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nadezha-umiraet-posledney");
-        c.DocumentTitle = "API-Info";
-        //Optionally, configure the Swagger UI route
-        //c.RoutePrefix = string.Empty; // Set Swagger UI at the app root URL
-    });
-    //app.UseSwaggerUI();
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<CustomUserDbContext>(options =>
+            options.UseSqlite("Data Source=SiteDB.db"));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nadezha-umiraet-posledney");
+                c.DocumentTitle = "API-Info";
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
