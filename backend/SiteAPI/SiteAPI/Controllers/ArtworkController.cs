deeb2355773125836d72e38eb94f797d57798d62
feniksg <33801134+feniksg@users.noreply.github.com>
2024-04-18 23:37:01 +0300

    Create Artwork Controller

---
+++ backend/SiteAPI/SiteAPI/Controllers/ArtworkController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiteAPI.Data;
+using SiteAPI.Models;
+
+namespace SiteAPI.Controllers
+{
+    [Route("api/artwork")]
+    [ApiController]
+    public class ArtworkController : ControllerBase
+    {
+        private readonly ArtworkDbContext _context;
+
+        public ArtworkController(ArtworkDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var arts = await _context.Artworks.ToListAsync();
+
+            if (arts == null) { return NotFound(); }
+
+            return Ok(arts);
+
+        }
+
+        [HttpPost]
+        
+        public async Task<IActionResult> Add(Artwork art)
+        {
+            await _context.Artworks.AddAsync(art);
+            await _context.SaveChangesAsync();
+
+            return Ok(art);
+        }
+    }
+}
