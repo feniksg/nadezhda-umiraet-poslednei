c0737659340d62058be98234f5f72de946188067
feniksg <33801134+feniksg@users.noreply.github.com>
2024-04-19 18:49:09 +0300

    fix(artworkcontroller.cs) fix foreign key

--- backend/SiteAPI/SiteAPI/Models/Artwork.cs
+++ backend/SiteAPI/SiteAPI/Models/Artwork.cs
@@ -9,15 +9,10 @@ namespace SiteAPI.Models
         public string? Series { get; set; }
         public string? Genre { get; set; }
         public required string ImageFilePath { get; set; } // Путь к файлу в папке media
-<<<<<<< Updated upstream
          
         //для свяхи с периодом жизни
         public int LifePeriodId { get; set; }//внешний ключ 
         public LifePeriod? LifePeriod { get; set; }//для программы чтобы обращаться Artwork.LifePeriod
-=======
 
-        public int LifePeriodId { get; set; }
-        public LifePeriod LifePeriod { get; set; }
->>>>>>> Stashed changes
     }
 }
